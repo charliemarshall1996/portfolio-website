02329a0d38392567596e14c9b9312c5397a46010
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-02-07 14:09:36 +0000

    created core models

--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Qualification(models.Model):
+    name = models.CharField(max_length=255)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=10, blank=True, null=True)
+    date_earned = models.DateField()
+    ongoing = models.BooleanField(default=False)
+    skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return self.name
