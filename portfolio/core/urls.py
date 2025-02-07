3aa006e76542376a46d7493070eab441cd12ca96
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-02-07 14:55:59 +0000

    created hero for home.html

--- portfolio/core/urls.py
+++ portfolio/core/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
-urlpatterns = []
+app_name = "core"
+
+urlpatterns = [
+    path("", views.home_view, name="home"),
+]
