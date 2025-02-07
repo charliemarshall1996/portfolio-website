53e926927229354573c1bf38b968f0595e04042e
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-02-07 15:39:44 +0000

    added about section

--- portfolio/core/admin.py
+++ portfolio/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+
+admin.site.register(models.Institution)
+admin.site.register(models.Skill)
+admin.site.register(models.Qualification)
--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -23,9 +23,9 @@ class Qualification(models.Model):
     name = models.CharField(max_length=255)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     grade = models.CharField(max_length=10, blank=True, null=True)
-    date_earned = models.DateField()
+    date_earned = models.DateField(null=True, blank=True)
     ongoing = models.BooleanField(default=False)
-    skills = models.ManyToManyField(Skill)
+    skills = models.ManyToManyField(Skill, blank=True)
 
     def __str__(self):
         return self.name
