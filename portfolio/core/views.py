ce52ccc20fb6e853f75ede938ae9a76eaced971a
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-02-07 14:14:58 +0000

    created home_view

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def home_view(request):
+    context = {
+        "qualifications": models.Qualification.objects.all(),
+        "skills": models.Skill.objects.all(),
+    }
+    return render(request, "core/home.html", context)
