9eeb64d4c47501608503c1cbde7e3c1e2fedb0ad
charlie marshall <charlie.marshall1996@gmail.com>
2025-03-28 03:18:17 +0000

    added base.html, company_list.html, home.html

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView, DetailView, CreateView
 
+from .forms import CompanyForm
+from .models import Company
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, 'crm/home.html')
+
+
+class CompanyListView(ListView):
+    model = Company
