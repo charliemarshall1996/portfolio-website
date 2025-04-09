c223907fb1b55568d56bf03f44113a9acf621894
charlie marshall <charlie.marshall1996@gmail.com>
2025-04-09 10:47:48 +0100

    fixed confirmation email

--- booking/views.py
+++ booking/views.py
@@ -21,7 +21,7 @@ def get_booking_confirmation_message(first_name, booking_date, booking_time):
             
             This a confirmation email that you have booked a meeting with me at {booking_time} on {booking_date}.
             I will be in touch shortly to provide a meeting link to confirm this.
-            I very much look forward to seeing how we can build the web app of your dreams!
+            I very much look forward to seeing how we can build the web app of your dreams.
             
             Many thanks,
             Charlie Marshall
@@ -53,10 +53,10 @@ class BookingAPI(views.APIView):
             first_name = serializer.data['first_name']
             date = serializer.data['date']
             start_time = serializer.data['time']
-            email = ['email']
+            email = serializer.data['email']
             message = get_booking_confirmation_message(
                 first_name, date, start_time)
-            send_mail("Meeting Confirmation", message=message,
+            send_mail("Meeting Confirmation Email - Charlie Marshall Web Development", message=message,
                       from_email="charlie@charlie-marshall.dev", recipient_list=[email])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
