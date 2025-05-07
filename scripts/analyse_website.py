7bff0d36fb1f74b548da52e756de547e426684e8
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-07 13:31:13 +0100

    changes to tasks

--- scripts/analyse_website.py
+++ scripts/analyse_website.py
@@ -1,29 +1,25 @@
 # website/tasks.py
 import logging
 from website.services.analyzer import WebsiteAnalyzer
-from . import models
+from website import models
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def analyse_website_marketing():
-    logger.info("Starting website analysis...")
+def run():
+    print("Starting website analysis...")
     analyzer = WebsiteAnalyzer()
     websites = list(models.Website.objects.all())
-    logger.debug("N WEBSITES: %s", str(len(websites)))
+    print("N WEBSITES: %s", str(len(websites)))
     anayzed_websites = [a.website.pk for a in models.Analysis.objects.all()]
 
     for website in websites:
         if website.pk not in anayzed_websites:
-            logger.info("Analyzing website %s", website.pk)
+            print("Analyzing website %s", website.pk)
             data = analyzer.run(website.url)
-            logger.debug("Website analyzed: %s", data)
+            print("Website analyzed: %s", data)
             analysis = models.Analysis.objects.create(
                 website=website, data=data)
             analysis.save()
             break
-
-
-if __name__ == "__main__":
-    analyse_website_marketing()
