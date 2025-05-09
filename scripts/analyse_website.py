ea7f58281cd91b2bcfb3f66e064e166d6e0200df
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-09 14:07:51 +0100

    updated break clause on analyse website

--- scripts/analyse_website.py
+++ scripts/analyse_website.py
@@ -17,12 +17,15 @@ def run():
     print("N WEBSITES: %s", str(len(websites)))
     anayzed_websites = [a.website.pk for a in models.Analysis.objects.all()]
 
+    n_analysed = 0
     for website in websites:
         if website.pk not in anayzed_websites:
+            n_analysed += 1
             print("Analyzing website %s", website.pk)
             data = analyzer.run(website.url)
             print("Website analyzed: %s", data)
             analysis = models.Analysis.objects.create(
                 website=website, data=data)
             analysis.save()
-            break
+            if n_analysed >= 10:
+                break
