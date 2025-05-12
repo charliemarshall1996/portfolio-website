f76e4f358ddb78d2275caae02aade05c9bd62063
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-13 00:25:36 +0100

    added functionality to add websites

--- crm/serializers.py
+++ crm/serializers.py
@@ -3,22 +3,23 @@ from rest_framework import serializers
 from . import models
 
 
+class WebsiteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Website
+        fields = "__all__"
+
+
 class ContactSerializer(serializers.ModelSerializer):
+    websites = WebsiteSerializer(many=True)
+
     class Meta:
         model = models.Contact
-        fields = [
-            'id',
-            'salutation',
-            'first_name',
-            'last_name',
-            'company',
-            'position',
-            'email',
-            'phone',
-            'mobile',
-            'linkedin',
-            'status',
-            'notes',
-            'created_at',
-            'updated_at',
-        ]
+        fields = "__all__"
+
+    def create(self, validated_data):
+        websites_data = validated_data.pop('websites', [])
+        contact = models.Contact.objects.create(**validated_data)
+        for website_data in websites_data:
+            models.Website.objects.create(contact=contact, **website_data)
+        return contact
