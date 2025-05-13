f3d565848eaf666c6c4c3fea36a55919c3d92168
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-13 03:21:21 +0100

    changes to SearchParameterSerializer

--- scrapers/serializers.py
+++ scrapers/serializers.py
@@ -3,6 +3,9 @@ from . import models
 
 
 class SearchParameterSerializer(serializers.ModelSerializer):
+    term = serializers.SerializerMethodField()
+    location = serializers.SerializerMethodField()
+
     class Meta:
         model = models.SearchParameter
         fields = [
@@ -12,3 +15,9 @@ class SearchParameterSerializer(serializers.ModelSerializer):
             'last_run_thomson',
             'last_run_freeindex'
         ]
+
+    def get_term(self, obj):
+        return obj.term.term if obj.term else None
+
+    def get_location(self, obj):
+        return obj.location.name if obj.location else None
