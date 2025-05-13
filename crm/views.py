bf0eb865028ea08a14bb034918b3e14fceea8556
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-13 07:56:56 +0100

    fixed authenticators

--- crm/views.py
+++ crm/views.py
@@ -3,14 +3,14 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ContactSerializer
 
 
 @api_view(["POST"])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
+@authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def add_contact(request):
     serializer = ContactSerializer(data=request.data)
--- home/views.py
+++ home/views.py
@@ -4,8 +4,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from django.http import JsonResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
 from django.shortcuts import render
 
 
