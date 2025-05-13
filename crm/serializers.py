eb53b3868cb67d9140966b4e6d87ca1aa2256e93
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-13 08:17:12 +0100

    fix add_contact view

--- crm/serializers.py
+++ crm/serializers.py
@@ -18,12 +18,15 @@ class ContactSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         websites_data = validated_data.pop('websites', [])
-        contact = models.Contact.objects.create(**validated_data)
+        contact, created = models.Contact.objects.get_or_create(
+            **validated_data)
+        contact.save()
         for website_data in websites_data:
             # Remove 'contact' if present in the incoming data
             website_data.pop('contact', None)
-            models.Website.objects.get_or_create(
+            website, created = models.Website.objects.get_or_create(
                 contact=contact,
                 url=website_data['url']
             )
+            website.save()
         return contact
