5780c58746b7297377edc3aecbba3f12cf83ecf2
charlie marshall <charlie.marshall1996@gmail.com>
2025-05-13 02:37:06 +0100

    ammended scrapers urls

--- scrapers/urls.py
+++ scrapers/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = "scrapers"
 
+
 urlpatterns = [
-    path("api/get-oldest-searchparameter/",
-         views.get_oldest_searchparameter, name="get_oldest_searchparameter"),
-    path("api/update-searchparameter/",
-         views.UpdateSearchRunView.as_view(), name="update-searchparameter")
+    path("api/searchparameter/<int:pk>/",
+         views.SearchParameterView.as_view(), name="searchparameter-update"),
+    path("api/searchparameter/<str:directory>/",
+         views.SearchParameterView.as_view(), name="searchparameter-get"),
 ]
--- scrapers/views.py
+++ scrapers/views.py
@@ -9,41 +9,36 @@ from .serializers import SearchParameterSerializer
 from django.utils.timezone import now
 
 
-class UpdateSearchRunView(APIView):
+class SearchParameterView(APIView):
     def post(self, request, pk):
         try:
             search_param = SearchParameter.objects.get(pk=pk)
         except SearchParameter.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        update_target = request.data.get("update_field")
+        update_target = request.data.get("directory")
 
         if update_target == "thomson":
             search_param.last_run_thomson = now()
         elif update_target == "freeindex":
             search_param.last_run_freeindex = now()
         else:
-            return Response({"detail": "Invalid update_field value."},
+            return Response({"detail": "Invalid directory value."},
                             status=status.HTTP_400_BAD_REQUEST)
 
         search_param.save()
         return Response(SearchParameterSerializer(search_param).data)
 
+    def get(self, request, directory):
 
-def get_oldest_searchparameter(request):
-    param = (
-        SearchParameter.objects
-        .filter(last_run_freeindex__isnull=False)
-        .order_by("last_run_freeindex")
-        .select_related("location", "term")
-        .first()
-    )
+        if directory == "thomson":
+            order = "last_run_thomson"
+        else:
+            return Response({"detail": "Invalid directory"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        search_param = SearchParameter.objects.all().order_by(order).first()
 
-    if not param:
-        return JsonResponse({"error": "No SearchParameters found."}, status=404)
+        if not search_param:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-    return JsonResponse({
-        "pk": param.pk,
-        "location": param.location.name,
-        "term": param.term.term
-    })
+        return Response(SearchParameterSerializer(search_param).data)
