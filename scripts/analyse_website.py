642817bb67b1627e2ded0ceb0fde448741056cc8
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-05-13 15:54:27 +0100

    handled None data in analyse_website

--- scripts/analyse_website.py
+++ scripts/analyse_website.py
@@ -11,21 +11,23 @@ logger.setLevel(logging.DEBUG)
 def run():
     if not settings.configured:
         settings.configure()
-    print("Starting website analysis...")
+    logger.info("Starting website analysis...")
     analyzer = WebsiteAnalyzer()
     websites = list(models.Website.objects.all())
-    print("N WEBSITES: %s", str(len(websites)))
+    logger.info("N WEBSITES: %s", str(len(websites)))
     anayzed_websites = [a.website.pk for a in models.Analysis.objects.all()]
 
     n_analysed = 0
     for website in websites:
         if website.pk not in anayzed_websites:
             n_analysed += 1
-            print("Analyzing website %s", website.pk)
+            logger.info("Analyzing website %s", website.pk)
             data = analyzer.run(website.url)
-            print("Website analyzed: %s", data)
-            analysis = models.Analysis.objects.create(
-                website=website, data=data)
-            analysis.save()
-            if n_analysed >= 10:
-                break
+            logger.info("Website analyzed: %s", data)
+
+            if data:
+                analysis = models.Analysis.objects.create(
+                    website=website, data=data)
+                analysis.save()
+                if n_analysed >= 10:
+                    break
