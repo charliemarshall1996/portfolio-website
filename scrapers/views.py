59c79e6f4af0410c76833fa11dad93932e9fd8e2
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-05-15 18:13:00 +0100

    ammended SearchParameterView get method to return nulls before SearchParameters with values

--- scrapers/views.py
+++ scrapers/views.py
@@ -33,12 +33,13 @@ class SearchParameterView(APIView):
 
     def get(self, request, directory):
         if directory == "thomson":
-            order = "last_run_thomson"
+            order = ['-last_run_thomson__isnull', 'last_run_thomson']
         else:
             return Response(
                 {"detail": "Invalid directory"}, status=status.HTTP_400_BAD_REQUEST
             )
-        search_param = SearchParameter.objects.all().order_by(order).first()
+
+        search_param = SearchParameter.objects.all().order_by(*order).first()
 
         if not search_param:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
