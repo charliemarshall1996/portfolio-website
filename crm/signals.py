107129d653126b7e9e1847620a040f21c6559991
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-05-19 15:09:08 +0100

    home page changes

--- crm/signals.py
+++ crm/signals.py
@@ -6,9 +6,9 @@ from models import SearchTerm, Campaign, CampaignSearchParemeter
 
 
 @receiver(signals.post_save, Campaign)
-def create_search_parameters_on_campaign_save(sender, instance, created):
+def create_search_parameters_on_campaign_save(sender, instance, **kwargs):
     for term in list(instance.vertical.search_terms):
         for loc in list(instance.locations):
-            search_param, created = CampaignSearchParemeter.objects.get_or_create(
-                location=loc, search_term=term)
+            search_param, created = CampaignSearchParemeter.objects.get_or_create(campaign=instance,
+                                                                                  location=loc, search_term=term)
             search_param.save()
